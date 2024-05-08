e7dd1adcf731918211cd4e8b5be48ea2cb3ab475
shangcom <dndbs91@gmail.com>
2024-05-08 16:33:07 +0900

    5_8_1

--- src/main/java/com/green/appec2/controller/SampleController.java
+++ src/main/java/com/green/appec2/controller/SampleController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//5_8_1
 @RestController
 @RequestMapping("/api/sample")
 public class SampleController {
